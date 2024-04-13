9634348c69df09fe85409e435fa3632dc729878b
Raniel <raniel.paula@fatec.sp.gov.br>
2024-04-13 17:15:00 -0300

    Ajustes de implementação

--- src/routes/doc.py
+++ src/routes/doc.py
@@ -9,12 +9,15 @@ from models.docs import Docs
 import os
 
 router = APIRouter()
-
+# C:\API - DomRock - BackEnd\BackEnd\API-6Semestre-Dom-Rock-Back-end
+# Caminho absoluto para o diretório 'uploads'
+path = os.path.abspath(os.getcwd()) 
+upload_path = os.path.join(path, 'uploads')
 
 @router.post("/inserir", description="Rota para inserir as informações do arquivo enviados via upload", status_code=200)
 async def create_doc(file: UploadFile = File(...), db: Session = Depends(get_db), response:Response = 200):
     file_ext = file.filename.split(".")[-1]
-    file_path = f"uploads/{file.filename}"
+    file_path = os.path.join(upload_path, file.filename)
     os.makedirs("uploads", exist_ok=True)
     with open(file_path, "wb") as f:
         content = await file.read()
@@ -28,7 +31,6 @@ async def create_doc(file: UploadFile = File(...), db: Session = Depends(get_db)
     db.commit()
     db.refresh(new_doc)  # Atualiza o objeto new_doc com as informações do banco de dados
     return new_doc
-
 
 @router.get("/get", description="Rota para buscar as informações de um documento por id")
 def get_doc_by_id(doc_id: int, db: Session = Depends(get_db)):
