1597f2f7133fd94d9493cf14f5b1b4f1697be03e
Raniel <raniel.paula@fatec.sp.gov.br>
2024-05-24 20:34:10 -0300

    Ajuste no limite de tamanho das reviews

--- src/models/reviews.py
+++ src/models/reviews.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP, Boolean, Text
 from models.base_class import Base
 from sqlalchemy.sql import func
 
@@ -7,7 +7,7 @@ class Reviews(Base):
     __tablename__ = 'reviews'
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(250), nullable=False)
-    review = Column(String(800), nullable=False)
+    review = Column(Text, nullable=False)
     rating = Column(Integer, nullable=False)
     recommend_product = Column(Boolean, nullable=False, default=False)
     created_at = Column(TIMESTAMP, nullable=False, default=func.now())
